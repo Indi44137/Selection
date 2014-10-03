383512f89c333fca91af4ba33ead80dd4a026eb6
Indi Knighton <44137@longroad.ac.uk>
2014-10-03 13:50:26 +0100

    Driving age

---
+++ Selection revision 2.py
@@ -0,0 +1,11 @@
+#Indi Knighton
+#01/10/2014
+#This program will work out whether you are old enough to drive
+
+driving_age = 18
+age = (int(input("Please enter your age here: ")))
+if age >= driving_age:
+    print("You are old enough to drive!")
+else:
+    print("You are too young to drive")
+    
