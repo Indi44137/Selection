211a47f4524f52792343458fc7dfdc56d2d10176
Indi Knighton <44137@longroad.ac.uk>
2014-10-03 08:51:25 +0100

    Selection revision 1

---
+++ Selection revision 1.py
@@ -0,0 +1,10 @@
+#Indi Knighton
+#30/09/2014
+#Program displaying the same number
+
+number1 = int(input("Please enter a number here: "))
+number2 = int(input("Please enter the same number here: "))
+if number1 == number2:
+    print("The two numbers you have selected were the same. Congradulations!")
+    
+
