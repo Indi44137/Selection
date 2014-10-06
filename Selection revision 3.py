bb686d9ba30755be267e9385855e27fc288850d1
Indi Knighton <44137@longroad.ac.uk>
2014-10-06 12:09:56 +0100

    Selection revision 3

---
+++ Selection revision 3.py
@@ -0,0 +1,9 @@
+#Indi Knighton
+#02/10/2014
+#A program which checks whether a number is in the range of 21-29
+
+number = int(input("Please enter a number here: "))
+if number >= 21 and number <= 29:
+    print("You have selected a number inside of this number range")
+
+      
