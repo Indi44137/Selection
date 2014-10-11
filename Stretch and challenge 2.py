6ddc6a0b4eabc9500812cec41cfa664b19ffac36
Indi <Indi@192.168.0.13>
2014-10-11 14:47:54 +0100

    Stretch and challenge

---
+++ Stretch and challenge 2.py
@@ -0,0 +1,34 @@
+#Indi Knighton
+#11/10/2014
+#This programme will take three separate integers and display the date with them
+
+month = int(input("Please enter a number between 1-12: "))
+day = int(input("Please enter a number between 1-31: "))
+year = int(input("Please enter the last two digits of a year in numbers here: "))
+
+
+if month == 1:
+    print("The month you entered is January {0} {1}".format(day, year))
+if month == 2:
+    print("The month you entered is February {0} {1}".format(day, year))
+if month == 3:
+    print("The month you entered is March {0} {1}".format(day, year)) 
+if month == 4:
+    print("The month you entered is April {0} {1}".format(day, year))
+if month == 5:
+    print("The month you entered is May {0} {1}".format(day, year))
+if month == 6:
+    print("The month you entered is June {0} {1}".format(day, year))
+if month == 7:
+    print("The month you entered is July {0} {1}".format(day, year))
+if month == 8:
+    print("The month you entered is August {0} {1}".format(day, year))
+if month == 9:
+    print("The month you entered is September {0} {1}".format(day, year))
+if month == 10:
+    print("The month you entered is October {0} {1}".format(day, year))
+if month == 11:
+    print("The month you entered is November {0} {1}".format(day, year))
+if month == 12:
+    print("The month you entered is December {0} {1}".format(day, year))
+    
