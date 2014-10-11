7c9473904bec5a807166586da446324099c6fbbc
Indi <Indi@192.168.0.13>
2014-10-11 10:03:14 +0100

    Stretch and challenge 1

---
+++ Stretch and challenge 1.py
@@ -0,0 +1,82 @@
+#Indi Knighton
+#11/10/2014
+#Display the month based on the integer picked and whether is was a leap year
+
+month = int(input("Please enter a month as a number between 1-12: "))
+year = int(input("Please enter a year here: "))
+if month == 1:
+    print("The month you entered is January")
+    if (year % 100 == 0) and (year % 400 == 0):
+        print("It is a leap year")
+    else:
+        print("It is not a leap year")
+elif month == 2:
+    print("The month you entered is February")
+    if (year % 100 == 0) and (year % 400 == 0):
+        print("It is a leap year")
+    else:
+        print("It is not a leap year")
+elif month == 3:
+    print("The month you entered is March")
+    if (year % 100 == 0) and (year % 400 == 0):
+        print("It is a leap year")
+    else:
+        print("It is not a leap year")
+elif month == 4:
+    print("The month you entered is April")
+    if (year % 100 == 0) and (year % 400 == 0):
+        print("It is a leap year")
+    else:
+        print("It is not a leap year")
+elif month == 5:
+    print("The month you entered is May")
+    if (year % 100 == 0) and (year % 400 == 0):
+        print("It is a leap year")
+    else:
+        print("It is not a leap year")
+elif month == 6:
+    print("The month you entered is June")
+    if (year % 100 == 0) and (year % 400 == 0):
+        print("It is a leap year")
+    else:
+        print("It is not a leap year")
+elif month == 7:
+    print("The month you entered is July")
+    if (year % 100 == 0) and (year % 400 == 0):
+        print("It is a leap year")
+    else:
+        print("It is not a leap year")
+elif month == 8:
+    print("The month you entered is August")
+    if (year % 100 == 0) and (year % 400 == 0):
+        print("It is a leap year")
+    else:
+        print("It is not a leap year")
+elif month == 9:
+    print("The month you entered is September")
+    if (year % 100 == 0) and (year % 400 == 0):
+        print("It is a leap year")
+    else:
+        print("It is not a leap year")
+elif month == 10:
+    print("The month you entered is October")
+    if (year % 100 == 0) and (year % 400 == 0):
+        print("It is a leap year")
+    else:
+        print("It is not a leap year")
+elif month == 11:
+    print("The month you entered is November")
+    if (year % 100 == 0) and (year % 400 == 0):
+        print("It is a leap year")
+    else:
+        print("It is not a leap year")
+elif month == 12:
+    print("The month you entered is December")
+    if (year % 100 == 0) and (year % 400 == 0):
+        print("It is a leap year")
+    else:
+        print("It is not a leap year")
+
+   
+
+
